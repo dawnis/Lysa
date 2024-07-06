31ce7aa75d4e4583e05ece68ccddd85476b8e0b2
dchow <dchow@box.com>
2024-07-06 14:09:10 -0400

    DRY for env classes and refined rewards

--- utils/environment_classes.py
+++ utils/environment_classes.py
@@ -25,14 +25,65 @@ def map_observation_8d(observation, obs_map):
 
 class GymnasiumCCEnv:
 
-    def __init__(self, environment_name, configuration, obs_space_dict, action_map_dict):
+    def __init__(self, environment_name, configuration, obs_space_dict, action_map_dict, agent_fitness_default=0):
         self.env_name = environment_name
+        self.agent_fitness_default = agent_fitness_default
         self.obs_map = obs_space_dict
         self.action_dict = action_map_dict
         self.env = gym.make(environment_name)
         self.evaluation_steps = 999
         self.observation_steps = 999
         self.configuration = configuration
+
+    def calculate_reward_delta(self, observation, reward):
+        return reward
+
+    def termination_penalty(self):
+        return 0
+
+    def assign_fitness(self, population, agent_idx, fitness):
+        fitness -= 0.1 * population.agent_complexity(agent_idx)
+        if fitness < 0:
+            population.set_agent_fitness(agent_idx, random.uniform(0, 1))
+        else:
+            population.set_agent_fitness(agent_idx, fitness)
+        return
+
+    def evaluate_agent(self, population, agent_idx):
+        observation, info = self.env.reset()
+
+        # In this task, there is a negative penalty for each step so start with the fitness goal
+        fitness = self.agent_fitness_default
+
+        for step in range(self.evaluation_steps):
+            mapped_observation = map_observation_8d(observation, self.obs_map)
+            population.agent_fwd(agent_idx, mapped_observation)
+            action_raw = population.agent_out(agent_idx)
+            action_discrete = self.map_action(action_raw[0])
+            observation, reward, terminated, truncated, info = self.env.step(action_discrete)
+
+            # if step % 10 == 0:
+            #     print(f"Observation: {observation}")
+            #     print(f"Mapped Observation: {mapped_observation}")
+            #     print(f"Raw output {action_raw}, chosen action: {action_discrete}")
+
+            reward_delta = self.calculate_reward_delta(observation, reward)
+
+            # if step % 10 == 0:
+            #     print(f"Observed reward is {reward_delta} for {observation}")
+
+            if reward_delta == reward_delta:
+                fitness += reward_delta #forward progress + clipped reward penalty - complexity penalty
+
+            if fitness != fitness:
+                print(f"Error! Observing NaN Fitness")
+
+            if terminated or truncated:
+                self.assign_fitness(population, agent_idx, fitness)
+                break
+
+        #print(f"Setting agent {agent_idx} fitness to {fitness}")
+        self.assign_fitness(population, agent_idx, fitness)
 
     def map_action (self, nn_output):
         """Maps output of NN to a discrete number for Gymanisum"""
@@ -65,6 +116,47 @@ class GymnasiumCCEnv:
     def write_agent(self, population, agent_idx, file_save_path):
         population.agent_checkpt(agent_idx, file_save_path)
 
+class AcrobotEnvironment(GymnasiumCCEnv):
+
+    def __init__(self, configuration):
+        obs_space_dictionary = {
+            "xpos": 0,
+            "ypos": 1,
+            "xvel": 2,
+            "yvel": 3,
+            "angle": 4,
+            "angular_vel": 5,
+        }
+
+        action_map_dictionary = {
+            "left": 0,
+            "noop": 1,
+            "right": 2
+        }
+        acrobat_action = defaultdict(lambda: 1, action_map_dictionary)
+
+        super().__init__("Acrobot-v1", configuration, obs_space_dictionary, acrobat_action, agent_fitness_default=500)
+
+class PendulumEnvironment(GymnasiumCCEnv):
+
+    def __init__(self, configuration):
+        obs_space_dictionary = {
+            "xpos": 0,
+            "ypos": 1,
+            "xvel": 2,
+            "yvel": 3,
+            "angle": 4,
+            "angular_vel": 5,
+        }
+
+        action_map_dictionary = {}
+        pendulum_action = defaultdict(lambda: 0, action_map_dictionary)
+
+        super().__init__("Pendulum-v1", configuration, obs_space_dictionary, pendulum_action, agent_fitness_default=16.3*200)
+
+    def map_action(self, nn_output):
+        return 2*nn_output
+
 class CartPoleEnvironment(GymnasiumCCEnv):
 
     def __init__(self, configuration):
@@ -81,54 +173,6 @@ class CartPoleEnvironment(GymnasiumCCEnv):
         cartpole_action = defaultdict(lambda: 0, action_map_dictionary)
         super().__init__("CartPole-v1", configuration, obs_space_dictionary, cartpole_action)
 
-    def evaluate_agent(self, population, agent_idx):
-        observation, info = self.env.reset()
-        fitness = 0
-        termination_penalty = min([100 * population.generation + 1, 1000])
-        step = 0
-
-        for _ in range(self.evaluation_steps):
-            step += 1
-            mapped_observation = map_observation_8d(observation, self.obs_map)
-
-
-            population.agent_fwd(agent_idx, mapped_observation)
-            action_raw = population.agent_out(agent_idx)
-            action_discrete = self.map_action(action_raw[0])
-
-            # if step % 10 == 0:
-            #     print(f"Observation: {observation}")
-            #     print(f"Mapped Observation: {mapped_observation}")
-            #     print(f"Raw output {action_raw}, chosen action: {action_discrete}")
-
-            observation, reward, terminated, truncated, info = self.env.step(action_discrete)
-
-            reward_delta = reward #sometimes there are NaNs, which crash the program
-
-            if (reward_delta == reward_delta) & (step > 9):
-                fitness += reward_delta #forward progress + clipped reward penalty - complexity penalty
-
-            if fitness != fitness:
-                print(f"Error! Observing NaN Fitness")
-
-            if terminated or truncated:
-                #penalize every termination with a constant penalty
-                step = 0
-                new_fitness = fitness - termination_penalty
-                fitness = max([new_fitness, 0])
-
-                observation, info = self.env.reset()
-
-        #print(f"Setting agent {agent_idx} fitness to {fitness}")
-
-        fitness -= 0.1 * population.agent_complexity(agent_idx)
-        
-        if fitness < 0:
-            population.set_agent_fitness(agent_idx, random.uniform(0, 1))
-        else:
-            population.set_agent_fitness(agent_idx, fitness)
-
-    
 class MountainCarEnvironment(GymnasiumCCEnv):
 
     def __init__(self, configuration):
@@ -141,38 +185,11 @@ class MountainCarEnvironment(GymnasiumCCEnv):
             "noop": 1,
             "right": 2,
         }
-        mc_action = defaultdict(lambda: 0, action_map_dictionary)
-        super().__init__("MountainCar-v0", configuration, obs_space_dictionary, mc_action)
+        mc_action = defaultdict(lambda: 1, action_map_dictionary)
+        super().__init__("MountainCar-v0", configuration, obs_space_dictionary, mc_action, agent_fitness_default=200)
 
-    def evaluate_agent(self, population, agent_idx):
-        observation, info = self.env.reset()
-        fitness = 0
+    def calculate_reward_delta(self, observation, reward):
+        postional_reward = max([0, observation[0] + 0.4])
+        fwd_velocity_reward = max([0, observation[1]])
+        return (postional_reward + fwd_velocity_reward)*10 + float(reward)
 
-        for _ in range(self.evaluation_steps):
-            mapped_observation = map_observation_8d(observation, self.obs_map)
-            population.agent_fwd(agent_idx, mapped_observation)
-            action_raw = population.agent_out(agent_idx)
-            action_discrete = self.map_action(action_raw[0])
-            observation, reward, terminated, truncated, info = self.env.step(action_discrete)
-
-            reward_delta = max([0, mapped_observation[0]])*10 + reward #sometimes there are NaNs, which crash the program
-
-            if reward_delta == reward_delta:
-                fitness += reward_delta #forward progress + clipped reward penalty - complexity penalty
-
-            if fitness != fitness:
-                print(f"Error! Observing NaN Fitness")
-                
-            if terminated or truncated:
-                observation, info = self.env.reset()
-
-        #print(f"Setting agent {agent_idx} fitness to {fitness}")
-
-        fitness -= 0.1 * population.agent_complexity(agent_idx)
-        
-        if fitness < 0:
-            population.set_agent_fitness(agent_idx, random.uniform(0, 1))
-        else:
-            population.set_agent_fitness(agent_idx, fitness)
-
-    
--- utils/evolve.py
+++ utils/evolve.py
@@ -3,28 +3,37 @@ from .environment_classes import *
 
 class evolution_chamber:
 
-    def __init__(self, environment):
+    def __init__(self, environment, checkpoint=None):
 
         self.configuration = {
-            "population_size": 200,
-            "survival_rate": 0.2,
+            "population_size": 2000,
+            "survival_rate": 0.05,
             "mutation_rate": 0.4,
             "input_size": 8,
+            "hidden_size": 2,
             "output_size": 1,
             "topology_mutation_rate": 0.4,
             "project_name": "Lysa",
             "project_directory": "/Users/dchow/git/Lysa/agents"
         }
 
-        self.population = evo_rl.PopulationApi(self.configuration)
+        if checkpoint is not None:
+            self.population = evo_rl.PopulationApi(self.configuration, checkpoint)
+        else:
+            self.population = evo_rl.PopulationApi(self.configuration)
 
-        self.max_generations = 1000
-        self.stopping_fitness = 400
+        self.max_generations = 200
+        self.min_generations = 10 #so we can reach the visualization checkpoint
+        self.stopping_fitness = 200
 
         if environment == 'CartPole':
             self.env = CartPoleEnvironment(self.configuration)
         elif environment == 'MountainCar':
             self.env = MountainCarEnvironment(self.configuration)
+        elif environment == 'Acrobot':
+            self.env = AcrobotEnvironment(self.configuration)
+        elif environment == 'Pendulum':
+            self.env = PendulumEnvironment(self.configuration)
         else:
             self.env = None
 
@@ -32,9 +41,11 @@ class evolution_chamber:
     def run_evolve(self):
 
         while self.population.generation < self.max_generations:
+
             for agent in range(self.configuration["population_size"]):
                 self.env.evaluate_agent(self.population, agent)
-            if self.population.fitness > self.stopping_fitness:
+
+            if (self.population.generation > self.min_generations) and (self.population.fitness > self.stopping_fitness):
                 break
 
             self.population.update_population_fitness()
